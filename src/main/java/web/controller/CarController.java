ef6636fc053c03901b6b6ea7baa8e78d5126506b
Cherevko Yuri <cherevko.work@gmail.com>
2025-07-25 21:45:07 +0300

    fixed path

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,14 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CreateCarList;
 
-import java.util.List;
 
 @Controller
 public class CarController {
-    CreateCarList createCarList;
+    CreateCarList createCarList = new CreateCarList();
     @GetMapping("/cars?count ≥ 5")
     public String showAllCar (Model model) {
         model.addAttribute("cars", createCarList.getCarList());
